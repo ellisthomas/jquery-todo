0d25cb32d896177c6ce781c8e38a4ae32ffee008
Ellis Thomas <ellisthomas80@gmail.com>
2017-04-29 22:25:53 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,65 +1,75 @@
 $(document).ready(function(){
 
-	$("#new-item").click(() => {
-		$(".list-container").addClass("hide");
-		$(".new-container").removeClass("hide");
+	$('#new-item').click(() => {
+		$('.list-container').addClass('hide');
+		$('.new-container').removeClass('hide');
 	});
 
-	$("#list-items").click(() => {
-		$(".new-container").addClass("hide");
-		$(".list-container").removeClass("hide");
+	$('#list-items').click(() => {
+		$('.new-container').addClass('hide');
+		$('.list-container').removeClass('hide');
 	});
-
-
 
 	//get todo
 	FbApi.getTodos().then(() => {
-		FbApi.writeToDom();
+		FbApi.writeDom();
 		countTask();
 	})
 	.catch((error) => {
 		console.log("getTodos Error", error);
 	});
 
-	$("#add-todo-button").click(() => {
-		let newTodo = {
-			isCompleted: false,
-			task: $("#add-todo-text").val()
-		};
-		console.log("newTodo", newTodo);
-		FbApi.addTodo(newTodo).then(() => {
-			$("#add-todo-text").val("");
-			$(".new-container").addClass("hide");
-			$(".list-container").removeClass("hide");
-			FbApi.writeToDom();
-			countTask();
-		}).catch(() => {
-			console.log("addTodo", error);
-		});
-	});
+  //add todo
+  $('#add-todo-button').click(() => {
+  	let newTodo = {
+  		isCompleted: false,
+  		task: $('#add-todo-text').val()
+  	};
+  	FbApi.addTodo(newTodo).then(() => {
+  		$('#add-todo-text').val("");
+  		$('.new-container').addClass('hide');
+			$('.list-container').removeClass('hide');
+  		FbApi.writeDom();
+  		countTask();
+  	}).catch((error) => {
+  		console.log("addTodo error", error);
+  	});
+  });
 
-	//add todo
-	//delete todo
-	//edit todo
-	//complete todos
-	$(".main-container").on("click", "input[type='checkbox']", (event) => {
-		console.log("event.target.id",event.target.id);
-		FbApi.checker().then(() => {
-			FbApi.writeToDom();
-			countTask();
-		}).catch((error) => {
-			console.log("check error", error);
-		});
-	});
+
+
+
+  //delete todo
+
+
+
+  //edit todo
+
+
+
+
+
+  //complete todos
+  $('.main-container').on('click', 'input[type="checkbox"]', (event)=>{
+  	FbApi.checker(event.target.id).then(() =>{
+  		FbApi.writeDom();
+  		countTask();
+  	}).catch((error) => {
+  		console.log("checker error", error);
+  	});
+  });
+
+
+
 
 
 	let countTask = () => {
-		let remainingTask = $("#incomplete-task li").length;
-		$("#counter").hide().fadeIn(300).html(remainingTask);
+		let remainingTasks = $('#incomplete-tasks li').length;
+		$('#counter').hide().fadeIn(300).html(remainingTasks);
 	};
 
 
 
 
-});
 
+});
