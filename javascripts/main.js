2f407d65b6be3f187bd9b3aea1df0f3bd1c77c9d
ellisthomas <ellisthomas80@gmail.com>
2017-05-02 20:51:02 -0500

    user registered

--- javascripts/main.js
+++ javascripts/main.js
@@ -87,11 +87,20 @@ $('.main-container').on('click', '.delete', (event) => {
   $("#registerButton").click(() => {
     let email = $("#inputEmail").val();
     let password = $("#inputPassword").val();
-    let usermane = $("#inputUsername").val();
+    let username = $("#inputUsername").val();
 
     let user = {email, password};
     FbApi.registerUser(user).then((response) => {
-      console.log("register response", response);
+      console.log("register response", response.uid);
+      let newUser = {
+        uid: response.uid,
+        username: username
+      };
+      FbApi.addUser(apiKeys, newUser).then((response) => {
+        console.log("addUser", response);
+      }).catch((error)=> {
+        console.log("error in addUser", error);
+      });
     }).catch((error) => {
       console.log("error in registerUser", error);
     });
--- javascripts/userCrud.js
+++ javascripts/userCrud.js
@@ -1,8 +1,20 @@
-var FbApi = ((cats) => {
+var FbApi = ((fish) => {
 
 	
+	fish.addUser = (keys, newUser) => {
+		return new Promise((resolve, reject) => {
+			$.ajax({
+				method: "POST",
+				url: `${keys.databaseURL}/user.json`,
+				data: JSON.stringify(newUser)
+			}).done((response) => {
+				resolve(response);
+			}).fail((error) => {
+				reject(error);
+			});
+		});
+	};
 
 
-
-	return cats;
+	return fish;
 })(FbApi || {});
